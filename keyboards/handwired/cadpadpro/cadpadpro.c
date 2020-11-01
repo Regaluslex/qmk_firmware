1528d481399f1bb0803a6461d9ca2ee9ea42c010
Regaluslex <lelldorin12@yahoo.fr>
2020-11-01 12:08:01 +0100

    adding handwired/cadpadpro

---
+++ keyboards/handwired/cadpadpro/cadpadpro.c
@@ -0,0 +1,32 @@
+/* Copyright 2020 regalex
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "cadpadpro.h"
+
+bool process_record_kb(uint16_t keycode, keyrecord_t *record) {
+    // put your per-action keyboard code here
+    // runs for every action, just before processing by the firmware
+    return process_record_user(keycode, record);
+}
+
+
+void keyboard_pre_init_kb(void) {
+    keyboard_pre_init_user();
+}
+
+void matrix_init_kb(void) {
+	matrix_init_user();
+}
---
+++ keyboards/handwired/cadpadpro/config.h
@@ -0,0 +1,60 @@
+/*
+Copyright 2020 regalex
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+#include "config_common.h"
+
+/* USB Device descriptor parameter */
+#define VENDOR_ID  0xFEED
+#define PRODUCT_ID 0x0504
+#define DEVICE_VER 0x0001
+#define MANUFACTURER Regalex
+#define PRODUCT cadpadpro
+#define DESCRIPTION A custom macropad
+
+/* key matrix size */
+#define MATRIX_ROWS 3
+#define MATRIX_COLS 6
+#define MATRIX_ROW_PINS {B2, B4, E6}
+#define MATRIX_COL_PINS { F7, B1, B3, B6, B5, D7}
+#define DIODE_DIRECTION COL2ROW
+#define ENCODERS_PAD_A { F5 }
+#define ENCODERS_PAD_B { F4 }
+#define BACKLIGHT_PIN C6
+#define BACKLIGHT_LEVELS 12
+#define BACKLIGHT_BREATHING
+#define BREATHING_PERIOD 6
+#define UNUSED_PINS
+#define OLED_DISPLAY_ADDRESS 0x3C
+#define OLED_TIMEOUT 60000
+//#define OLED_DISABLE_TIMEOUT
+#define SSD1306OLED
+#define DEBOUNCE 5
+#define TAPPING_TERM 200
+
+/* disable these deprecated features by default */
+//#define NO_ACTION_MACRO
+//#define NO_ACTION_FUNCTION
+
+
+#ifndef NO_DEBUG
+#define NO_DEBUG
+#endif // !NO_DEBUG
+#if !defined(NO_PRINT) && !defined(CONSOLE_ENABLE)
+#define NO_PRINT
+#endif // !NO_PRINT
