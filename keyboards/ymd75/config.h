dc6f175d778ec98ca23803891887205ea3f323d0
Regaluslex <73775467+Regaluslex@users.noreply.github.com>
2021-01-05 07:08:10 +0100

    Update config.h

--- keyboards/ymd75/config.h
+++ keyboards/ymd75/config.h
@@ -30,7 +30,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define BACKLIGHT_LEVELS 12
 
 #define RGB_DI_PIN E2
-#define RGBLED_NUM 16
+#define RGBLED_NUM 14
 #define RGBLIGHT_ANIMATIONS
 #define RGBLIGHT_HUE_STEP 12
 #define RGBLIGHT_SAT_STEP 15
